8fe89a67439d77d5d92448443efde13c6420dc25
bojanS94 <eastcodewestcode94@gmail.com>
2021-04-08 12:41:01 +0200

    Color scheme changed

--- js/app.js
+++ js/app.js
@@ -1,25 +1,23 @@
 //Hover functions
-
 let profileBox = document.querySelector(".user-nav__user");
 let userInfo = document.querySelector(".user-nav__info");
 let galleryBox = document.querySelectorAll(".gallery__item");
 let galleryTxt = document.querySelectorAll(".gallery__text");
 
-profileBox.addEventListener('mouseenter', () => {
+let hoverEnter = () => {
+    let styles = {
+        top: "100%",
+        zIndex: 10
+    };
+    Object.assign(userInfo.style, styles);
+}
 
-    userInfo.style.top = "100%";
-    userInfo.style.zIndex = "10";
-
-});
-
-profileBox.addEventListener('mouseleave', () => {
-
-    userInfo.style.top = "90%";
-    userInfo.style.zIndex = "-1";
-
-});
-
-galleryBox.forEach(el => {
-
-
-})
+let hoverLeave = () => {
+    let styles = {
+        top: "0%",
+        zIndex: -1
+    };
+    Object.assign(userInfo.style, styles);
+}
+profileBox.addEventListener('mouseenter', hoverEnter, false);
+profileBox.addEventListener('mouseleave', hoverLeave, false);
